966bf0bcd41e86665d908f77b9ca28f9c49c66d4
Carpenteri1 <niclastimle@protonmail.com>
2021-05-20 16:37:16 +0200

    SearchPageAlmostDone

--- CampusBokningsSystem/wwwroot/js/site.js
+++ CampusBokningsSystem/wwwroot/js/site.js
@@ -2,3 +2,32 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+let date = new Date();
+
+$("#expandingDiv").hide();
+$("#HiddenListItem").hide();
+//function for dropdown parameter
+$(document).ready(function () {
+    $("#hideshowbutton").click(function () {
+        $("#expandingDiv").toggle("slow");
+    });
+    $("#hideshowlistitem").click(function () {
+        $("#HiddenListItem").toggle("slow");
+    });
+
+});
+
+
+$(document).ready(function () {
+    var span = document.getElementById('LiveTime');
+
+    function time() {
+        var d = new Date();
+        var m = d.getMinutes();
+        var h = d.getHours();
+        span.textContent =
+            ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2);
+    }
+
+    setInterval(time, 1000);
+});
